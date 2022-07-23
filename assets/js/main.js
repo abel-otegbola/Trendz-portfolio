f27549b0cdfe9e564ca9dfa690ed3c69ef545e36
abel-otegbola <abel15655@gmail.com>
2022-07-23 12:23:26 +0100

    added new preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,3 +34,41 @@ if (preloader) {
         navbar.classList.remove("scrolled")
     }
 })
+
+
+ /**
+ * glightbox
+ */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+
+  /**
+ * portfolio filter
+ */ 
+let portfolioFilters = document.querySelectorAll('#portfolio-flters li'); 
+let portfolioGallery = document.querySelectorAll('#projects .gallery .project'); 
+for(let i=0; i<portfolioFilters.length; i++) {
+    portfolioFilters[i].addEventListener('click', function(e) {
+        e.preventDefault();
+        for(let j=0; j<portfolioFilters.length; j++) {
+            portfolioFilters[j].classList.remove("active")
+        }
+        portfolioFilters[i].classList.add("active")
+        let dataFilter = portfolioFilters[i].getAttribute("data-filter")
+        for(let k=0; k<portfolioGallery.length; k++) {
+            if(dataFilter === "*") {
+                portfolioGallery[k].style.display = "block"   
+            }
+            else if(portfolioGallery[k].getAttribute("data-filter") === dataFilter) {
+                portfolioGallery[k].style.display = "block"
+            }
+            else {  
+                portfolioGallery[k].style.display = "none"   
+            }
+        }
+        
+        AOS.refresh()
+    });
+}
